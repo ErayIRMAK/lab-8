2cfb8d79842022822807943238e07aecd1de175e
ErayIRMAK <2010206056@ogrenci.karabuk.edu.tr>
2022-12-06 23:15:43 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+
+int main() {
+
+    // canerozcan.net/lab8z.pdf sorulari ERAY IRMAK
+
+    /*     SORU 1
+    int sayi1, sayi2;
+
+    printf("Sirasiyla 2 sayi giriniz.");
+    scanf("%d %d",&sayi1,&sayi2);
+    printf("Girdiginiz ilk sayi %d ikinci sayi ise %d idir.",sayi1,sayi2);
+    */
+
+    //====================================================================================
+
+    /*      SORU 2
+    float yaricap,alan;
+
+    printf("Alaninin hesaplanmasini istediginiz dairenin yaricapini giriniz.");
+    scanf("%d",&yaricap);
+
+    alan = 3.14*yaricap*yaricap;
+    printf("Dairenin alani %d idir",alan);
+
+    */
+
+    //====================================================================================
+
+    /*      SORU 3
+    int N,sonuc;
+    printf("Cift mi tek mi oldugunu ogrenmek istedigin sayiyi gir.");
+    scanf("%d",&N);
+
+    if(N%2 == 0) printf("Girmis oldugunuz sayi cifttir");
+    else printf("Girmis oldugunuz sayi tek sayidir.");
+    */
+
+    //====================================================================================
+
+    /*      SORU 4
+    int sicaklik;
+
+    printf("Lutfen sicakligi giriniz");
+    scanf("%d",&sicaklik);
+
+    if(sicaklik>0) printf("Sicaklik donma noktasinin ustunde");
+    else if(sicaklik==0) printf("Sicaklik donma noktasinda");
+    else printf("Sicaklik donma noktasinin altinda");
+    */
+
+    //====================================================================================
+
+    /*      SORU 5
+    int Fahrenheit,Celsius;
+
+
+    printf("Lutfen fahrenheit degerini giriniz.");
+    scanf("%d",&Fahrenheit);
+
+    Celsius = (Fahrenheit-32)*5/9;
+    printf("Girmis oldugunuz Fahrenheit degerinin Celcius karsiligi %d dir",Celsius);
+    */
+
+    //====================================================================================
+
+    /*      SORU 6
+    float feet,cm;
+
+    printf("Lutfen feet degerini giriniz.");
+    scanf("%f",&feet);
+
+    cm = (feet*30.48);
+    printf("Girmis oldugunuz feet degerinin cm karsiligi %f dir",cm);
+    */
+
+    //====================================================================================
+
+    /*      SORU 7
+    int A,B,C;
+
+    printf("Lutfen A B C degerlerini sirasiyla giriniz.");
+    scanf("%d %d %d",&A,&B,&C);
+
+    if(A>B && A>C) printf("En buyuk sayi A(%d) idir",A);
+    else if (B>A && B>C) printf("En buyuk sayi B(%d) idir",B);
+    else printf("En buyuk sayi C(%d) idir",C);
+    */
+
+
+
+
+
+
+
+
+
+
+
+}
